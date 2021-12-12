4bbba030604bf8072e7b26b13209a0669dae473b
dox365 <dox365@gmail.com>
2021-12-12 20:20:32 -0300

    Sistema pecas atividade 2 UC8

---
+++ sistema_peca.js
@@ -0,0 +1,33 @@
+et PesoPeca = 100;
+let ListaPecas = ['peca1', 'peca2', 'peca3'];
+let NomePecas = "Bateria";
+
+
+if (PesoPeca >= 100) {
+    
+    console.log("Cadastro permitido. Peso maior do que 100g.");
+}
+else {
+    console.log("Peças com peso menor do que 100g.");
+}
+
+
+if (listaPecas.length > 10) {
+
+    console.log("Caixa sem capacidade. Permitido 10 peças por caixas.");
+    
+}
+
+else
+{
+    console.log("Cadastro permitido. Há espaço nas caixas.");
+}
+
+if (nomePecas.length > 3) {
+    console.log("Cadastro permitido. Nome da peça está correto.");
+}
+else{
+    console.log("Cadastro inválido. Nome da peça incompleto.");
+}
+
+
